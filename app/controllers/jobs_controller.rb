e3b16f1352384ac0c8d35d998e17d4427006065b
kai <328458211@qq.com>
2017-03-10 15:13:32 +0800

    add job#new/update controller

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,26 @@
+class JobsController < ApplicationController
+
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+
+  end
+
+  def update
+    @job = Job.new[job_params]
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
