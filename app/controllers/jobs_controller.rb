11d8b9ee49bd94d4db0a202d0c10beb8fdb30bf6
kai <328458211@qq.com>
2017-04-21 10:22:30 +0800

    job#show beauty and will_pagine in job#index

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,14 +12,14 @@ class JobsController < ApplicationController
   end
 
   def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC").paginate(:page => params[:page], :per_page => 5 )
     @jobs = case params[:order]
     when 'by_lower_bound'
-      Job.published.order('wage_lower_bound DESC')
+      Job.published.order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 5 )
     when 'by_upper_bound'
-      Job.published.order('wage_upper_bound DESC')
+      Job.published.order('wage_upper_bound DESC').paginate(:page => params[:page], :per_page => 5 )
     else
-      Job.published.recent
+      Job.published.recent.paginate(:page => params[:page], :per_page => 5 )
     end
   end
 
