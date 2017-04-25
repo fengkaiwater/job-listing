411030fb5f33a3688372f99e325227d910552023
kai <328458211@qq.com>
2017-04-25 11:29:13 +0800

    validation to wage

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,7 @@
 class Job < ApplicationRecord
   validates :title, presence: true
+  validates :wage_upper_bound, presence: true
+  validates :wage_upper_bound, numericality: { greater_than: 1500 }
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 1500 }
 end
