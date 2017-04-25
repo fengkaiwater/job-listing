fa89cd77e5d55436cba18d92f920ed630fb3d459
kai <328458211@qq.com>
2017-04-25 09:18:41 +0800

    job CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,48 @@
 class JobsController < ApplicationController
+
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to jobs_path, notice: "Create Success!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path, notice: "Update Success!"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path, alert: "Delete Success!"
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
 end
